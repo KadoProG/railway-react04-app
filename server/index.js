f250dcbd46f46754c87393d48107d5f1b2a58f28
KadoProG <splatrain@gmail.com>
2024-02-07 20:01:23 +0900

    webpackの導入

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var app = express()
-var port = 9000
+// const express = require('express')
+import express from 'express'
+const app = express()
+const port = 9000
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-app.listen(port, function () {
-  console.log('\n\nsuccess!')
-  console.log(`URL:         http://localhost:${port}\n\n`)
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`\n\nsuccess!\nURL:\t\t\thttp://localhost:${port}\n\n`)
 })
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node', // サーバーサイドを対象に設定
+  entry: './server/index.js',
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+}
